d17cfb5af2352129d66f66367516dd1cbd895909
Aletf7 <asuagar94@gmail.com>
2025-11-12 15:04:54 +0000

    wip config firebase to auth

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCTzZOc1WoPqIXBMKU8IN4ZQCVQ8xYqx7A",
+    authDomain: "appshurite.firebaseapp.com",
+    projectId: "appshurite",
+    storageBucket: "appshurite.firebasestorage.app",
+    messagingSenderId: "32817552550",
+    appId: "1:32817552550:web:3b21adbf8e2eae591aa867",
+    measurementId: "G-DFFH65ERR3"
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAnalytics(() => getAnalytics())
   ]
 });
